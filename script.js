f5db7785806940f14457d4c93676c6ff900fb9fd
Ruby4033 <rupika200518@gamil.com>
2025-09-28 17:12:23 +0530

    commit

--- script.js
+++ script.js
@@ -44,3 +44,25 @@ document.getElementById("recipeForm").addEventListener("submit", function(event)
     });
 });
 
+// dark mode 
+const toggleBtn = document.getElementById("darkModeToggle");
+const body = document.body;
+
+
+if (localStorage.getItem("darkMode") === "enabled") {
+  body.classList.add("dark-mode");
+  toggleBtn.textContent = "☀️"; 
+}
+
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("darkMode", "enabled");
+    toggleBtn.textContent = "☀️"; 
+    localStorage.setItem("darkMode", "disabled");
+    toggleBtn.textContent = "🌙"; 
+  }
+});
+
